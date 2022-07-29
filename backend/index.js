8c71cfdc44c1edf3bd7eb24655089a3644fd0718
Curtis Bridge <c.bridge0496@gmail.com>
2022-07-29 19:52:38 -0400

    trying to fix card deleting problem

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const { Player } = require('./db/models');
-const player = require("./db/models/player");
+const { Player } = require('./models');
 
 const PORT = process.env.PORT || 3001;
 
@@ -22,7 +21,11 @@ app.put('/wins/:id(\\d+)', async (req, res) => {
   console.log(player.wins)
   const numberOfWins = player.wins += 1
   const update = await player.update({ wins: numberOfWins })
-  return res.json(update)
+
+  const updated = await Player.findAll({
+    order: [["wins", 'desc']]
+  })
+  return res.json(updated)
 })
 
 app.listen(PORT, () => {
--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -68,6 +68,7 @@ function Game(props) {
     if (round['playerTwo']) {
       setPlayerTwosHand([...playerTwosHand, ...pot, ...round['playerTwo']])
     }
+
   }
 
   let restart = () => {
@@ -82,6 +83,12 @@ function Game(props) {
     }
   };
 
+  const superSim = ()=>{
+    while(playerOnesHand.length && playerTwosHand.length){
+      startRound()
+    }
+  }
+
   return (
     <div
       onKeyPress={handleKeypress}
@@ -94,6 +101,7 @@ function Game(props) {
           <p>Player Ones Cards: {playerOnesHand.length}</p>
           <p>Player Twos Cards: {playerTwosHand.length}</p>
           <button id='start-round' onClick={() => startRound()}>Start Round</button>
+          <button onClick={()=> superSim()}>Super Sim</button>
 
         </div>
       )
